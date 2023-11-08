2bdc279e9d5e74b2b31ede94270c9ac07d0fe528
potato0221 <wnqo10@naver.com>
2023-11-08 16:58:21 +0900

    t7 pass

--- src/main/java/com/ll/global/app/App.java
+++ src/main/java/com/ll/global/app/App.java
@@ -1,10 +1,15 @@
 package com.ll.global.app;
 
+import com.ll.domain.quotation.quotation.entity.Quotation;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
 
     private final Scanner scanner;
+
 
     public App(Scanner scanner) {
         this.scanner = scanner;
@@ -13,6 +18,7 @@ public class App {
     public void run() {
         System.out.println("== 명언 앱 ==");
 
+        List<Quotation> quotations=new ArrayList<>();
         long lastQuotationId=0;
 
         while (true){
@@ -26,12 +32,24 @@ public class App {
                 final String authorName=scanner.nextLine().trim();
                 final long id=++lastQuotationId;
 
+                Quotation quotation=new Quotation(id,authorName,content);
+                quotations.add(quotation);
+
                 System.out.printf("%d번 명언이 등록 되었습니다.",(id));
             }else if(cmd.equals("목록")){
                 System.out.println("번호 / 작가 / 명언");
                 System.out.println("--------------------");
-                System.out.println("2 / 작자미상 / 과거에 집착하지 마라.");
-                System.out.println("1 / 작자미상 / 현재를 사랑하라.");
+
+                quotations
+                        .reversed()
+                        .forEach(
+                                quotation -> System.out.printf(
+                                        "%d / %s / %s",
+                                        quotation.getId(),
+                                        quotation.getAuthorName(),
+                                        quotation.getContent()
+                                )
+                        );
 
             }else if(cmd.equals("종료")) return;
 
