170f59f61b8a1645b04a2f9e5cebacbfd1f6446d
potato0221 <wnqo10@naver.com>
2023-11-12 16:18:26 +0900

    t2 refectoring

--- src/test/java/com/ll/standard/util/UtTest.java
+++ src/test/java/com/ll/standard/util/UtTest.java
@@ -1,5 +1,7 @@
 package com.ll.standard.util;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -7,30 +9,29 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class UtTest {
 
+    private final String filePath = "temp/test.txt";
+
+    @BeforeEach
+    void beforeEach() {
+        Ut.file.save(filePath, "내용");
+    }
+
+    @AfterEach
+    void afterEach() {
+        Ut.file.delete(filePath);
+    }
+
     @Test
     @DisplayName("파일 생성, 삭제")
-    void t1(){
-        String filePath="temp/test.txt";
-        Ut.file.save(filePath,"내용");
-
+    void t1() {
         assertThat(Ut.file.exists(filePath)).isTrue();
-
-        Ut.file.delete(filePath);
-
     }
 
     @Test
     @DisplayName("파일 내용 읽기")
-    void t2(){
-        String filePath="temp/test.txt";
-        Ut.file.save(filePath,"내용");
-
-
-        final String content=Ut.file.getContents(filePath);
+    void t2() {
+        final String content = Ut.file.getContents(filePath);
 
         assertThat(content).isEqualTo("내용");
-
-        Ut.file.delete(filePath);
-
     }
 }
