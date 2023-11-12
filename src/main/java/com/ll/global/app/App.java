b0b61daaba034b2639be8986ab19642c40fc6ec0
potato0221 <wnqo10@naver.com>
2023-11-12 14:18:13 +0900

    mvc w1

--- src/main/java/com/ll/global/app/App.java
+++ src/main/java/com/ll/global/app/App.java
@@ -10,17 +10,17 @@ import java.util.Scanner;
 public class App {
 
     private final Scanner scanner;
+    final List<Quotation> quotations;
+    long lastQuotationId;
 
 
     public App(Scanner scanner) {
         this.scanner = scanner;
+        quotations = new ArrayList<>();
+        lastQuotationId = 0;
     }
-
     public void run() {
         System.out.println("== 명언 앱 ==");
-
-        List<Quotation> quotations = new ArrayList<>();
-        long lastQuotationId = 0;
 
         while (true) {
             final String cmd = scanner.nextLine().trim();
@@ -72,17 +72,7 @@ public class App {
                 }
 
                 case "등록" -> {
-                    System.out.print("명언 : ");
-                    final String content = scanner.nextLine().trim();
-                    System.out.print("작가 : ");
-                    final String authorName = scanner.nextLine().trim();
-
-                    final long id = ++lastQuotationId;
-
-                    Quotation quotation = new Quotation(id, authorName, content);
-                    quotations.add(quotation);
-
-                    System.out.println("%d번 명언이 등록 되었습니다.".formatted(id));
+                    actionWrite();
                 }
                 case "목록" -> {
                     System.out.println("번호 / 작가 / 명언");
@@ -109,5 +99,19 @@ public class App {
         }
 
 
+    }
+
+    private void actionWrite() {
+        System.out.print("명언 : ");
+        final String content = scanner.nextLine().trim();
+        System.out.print("작가 : ");
+        final String authorName = scanner.nextLine().trim();
+
+        final long id = ++lastQuotationId;
+
+        Quotation quotation = new Quotation(id, authorName, content);
+        quotations.add(quotation);
+
+        System.out.println("%d번 명언이 등록 되었습니다.".formatted(id));
     }
 }
