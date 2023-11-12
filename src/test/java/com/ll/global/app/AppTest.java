f6f4201b95b0c766c7cf8ec93897ff6465526642
potato0221 <wnqo10@naver.com>
2023-11-12 13:30:10 +0900

    t10

--- src/test/java/com/ll/global/app/AppTest.java
+++ src/test/java/com/ll/global/app/AppTest.java
@@ -186,6 +186,33 @@ public class AppTest {
 
     }
 
+    @Test
+    @DisplayName("수정")
+    void t10() {
+
+        String out = run("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                등록
+                과거에 집착하지 마라.
+                홍길동
+                수정?id=2
+                과거에 집착하지 마라!
+                홍길동님
+                목록
+                """);
+
+        assertThat(out)
+                .contains("번호 / 작가 / 명언")
+                .contains("--------------------")
+                .contains("2 / 홍길동님 / 과거에 집착하지 마라!")
+                .doesNotContain("2 / 홍길동 / 과거에 집착하지 마라.")
+                .contains("2번 명언이 수정 되었습니다.")
+                .contains("1 / 작자미상 / 현재를 사랑하라.");
+
+    }
+
 
 
 
