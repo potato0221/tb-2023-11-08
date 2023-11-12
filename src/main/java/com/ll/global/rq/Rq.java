dfa87cba6a66c5b88ccbc05f23aad90321ae8638
potato0221 <wnqo10@naver.com>
2023-11-12 12:27:01 +0900

    RqTest t3 pass

--- src/main/java/com/ll/global/rq/Rq.java
+++ src/main/java/com/ll/global/rq/Rq.java
@@ -1,11 +1,25 @@
 package com.ll.global.rq;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Rq {
     private final String action;
+    private final String queryString;
+    private Map<String,String> params;
 
     public Rq(String cmd) {
-        final String[] cmdBIts=cmd.split("\\?",2);
-        action=cmdBIts[0];
+        final String[] cmdBIts = cmd.split("\\?", 2);
+        action = cmdBIts[0];
+        queryString = cmdBIts.length == 2 ? cmdBIts[1].trim() : "";
+        params=new HashMap<>();
+
+        if(queryString.isBlank()) return;
+
+        final String[] paramBits=queryString.split("=",2);
+        String paramName=paramBits[0].trim();
+        String paramValue=paramBits[1].trim();
+        params.put(paramName,paramValue);
     }
 
     public String getAction() {
@@ -13,7 +27,7 @@ public class Rq {
         return action;
     }
 
-    public String getParameter(String 이름) {
-        return "Paul";
+    public String getParameter(String paramName) {
+        return params.get(paramName);
     }
 }
