2ff0dd5bdf7f431566761019d6a7a226fe6b4fb8
potato0221 <wnqo10@naver.com>
2023-11-12 12:34:47 +0900

    RqTest t4 pass

--- src/main/java/com/ll/global/rq/Rq.java
+++ src/main/java/com/ll/global/rq/Rq.java
@@ -1,25 +1,34 @@
 package com.ll.global.rq;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Rq {
     private final String action;
     private final String queryString;
-    private Map<String,String> params;
+    private Map<String, String> params;
 
     public Rq(String cmd) {
         final String[] cmdBIts = cmd.split("\\?", 2);
         action = cmdBIts[0];
         queryString = cmdBIts.length == 2 ? cmdBIts[1].trim() : "";
-        params=new HashMap<>();
+        params = new HashMap<>();
 
-        if(queryString.isBlank()) return;
+        if (queryString.isBlank()) return;
 
-        final String[] paramBits=queryString.split("=",2);
-        String paramName=paramBits[0].trim();
-        String paramValue=paramBits[1].trim();
-        params.put(paramName,paramValue);
+        Arrays
+                .stream(queryString.split("&"))
+                .forEach(param -> {
+                    final String[] paramBits = param.split("=", 2);
+                    String paramName = paramBits[0].trim();
+                    String paramValue = paramBits[1].trim();
+
+                    params.put(paramName, paramValue);
+                });
+
+
+
     }
 
     public String getAction() {
