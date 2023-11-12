44aeebab68c6038a8533ea01ec74195f085ed8e2
potato0221 <wnqo10@naver.com>
2023-11-12 14:19:31 +0900

    mvc w2

--- src/main/java/com/ll/global/app/App.java
+++ src/main/java/com/ll/global/app/App.java
@@ -75,20 +75,7 @@ public class App {
                     actionWrite();
                 }
                 case "목록" -> {
-                    System.out.println("번호 / 작가 / 명언");
-                    System.out.println("----------------------");
-
-                    quotations
-                            .reversed()
-                            .forEach(
-                                    quotation -> System.out.println(
-                                            "%d / %s / %s".formatted(
-                                                    quotation.getId(),
-                                                    quotation.getAuthorName(),
-                                                    quotation.getContent()
-                                            )
-                                    )
-                            );
+                    actionShowList();
                 }
                 case "종료" -> {
                     return;
@@ -99,6 +86,24 @@ public class App {
         }
 
 
+    }
+
+    private void actionShowList() {
+
+        System.out.println("번호 / 작가 / 명언");
+        System.out.println("----------------------");
+
+        quotations
+                .reversed()
+                .forEach(
+                        quotation -> System.out.println(
+                                "%d / %s / %s".formatted(
+                                        quotation.getId(),
+                                        quotation.getAuthorName(),
+                                        quotation.getContent()
+                                )
+                        )
+                );
     }
 
     private void actionWrite() {
