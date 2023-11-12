003a5f0b2d5b9dd9cb9f057734b8cb5bb325325d
potato0221 <wnqo10@naver.com>
2023-11-12 14:48:00 +0900

    mvc w5

--- src/main/java/com/ll/global/app/App.java
+++ src/main/java/com/ll/global/app/App.java
@@ -1,26 +1,24 @@
 package com.ll.global.app;
 
-import com.ll.domain.quotation.quotation.entity.Quotation;
+import com.ll.domain.quotation.quotation.controller.QuotationController;
 import com.ll.global.rq.Rq;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class App {
 
     private final Scanner scanner;
-    final List<Quotation> quotations;
-    long lastQuotationId;
+
 
 
     public App(Scanner scanner) {
         this.scanner = scanner;
-        quotations = new ArrayList<>();
-        lastQuotationId = 0;
+
     }
     public void run() {
         System.out.println("== 명언 앱 ==");
+
+        final QuotationController quotationController=new QuotationController(scanner);
 
         while (true) {
             final String cmd = scanner.nextLine().trim();
@@ -30,18 +28,18 @@ public class App {
 
             switch (action) {
                 case "삭제" -> {
-                    actionRemove(rq);
+                    quotationController.actionRemove(rq);
                 }
 
                 case "수정" -> {
-                    actionModify(rq);
+                    quotationController.actionModify(rq);
                 }
 
                 case "등록" -> {
-                    actionWrite();
+                    quotationController.actionWrite();
                 }
                 case "목록" -> {
-                    actionShowList();
+                    quotationController.actionShowList();
                 }
                 case "종료" -> {
                     return;
@@ -54,75 +52,5 @@ public class App {
 
     }
 
-    private void actionRemove(Rq rq) {
-        final long id = rq.getParameterAsLong("id", 0);
 
-        quotations
-                .stream()
-                .filter(_quotation -> _quotation.getId() == id)
-                .findFirst()
-                .ifPresentOrElse(
-                        quotation -> {
-                            quotations.remove(quotation);
-                            System.out.printf("%d번 명언이 삭제 되었습니다.", id);
-                        },
-                        () -> System.out.printf("%d번 명언은 존재하지 않습니다.", id)
-                );
-    }
-
-    private void actionModify(final Rq rq) {
-        final long id = rq.getParameterAsLong("id", 0);
-        quotations
-                .stream()
-                .filter(_quotation -> _quotation.getId() == id)
-                .findFirst()
-                .ifPresentOrElse(
-                        quotation -> {
-                            System.out.printf("명언(기존) : %s", quotation.getContent());
-                            System.out.println("명언 : ");
-                            final String content = scanner.nextLine().trim();
-                            System.out.printf("작가(기존) : %s", quotation.getAuthorName());
-                            System.out.println("작가 : ");
-                            final String authorName = scanner.nextLine().trim();
-
-                            quotation.setContent(content);
-                            quotation.setAuthorName(authorName);
-
-                            System.out.printf("%d번 명언이 수정 되었습니다.", id);
-                        },
-                        () -> System.out.printf("%d번 명언은 존재하지 않습니다.", id)
-                );
-    }
-
-    private void actionShowList() {
-
-        System.out.println("번호 / 작가 / 명언");
-        System.out.println("----------------------");
-
-        quotations
-                .reversed()
-                .forEach(
-                        quotation -> System.out.println(
-                                "%d / %s / %s".formatted(
-                                        quotation.getId(),
-                                        quotation.getAuthorName(),
-                                        quotation.getContent()
-                                )
-                        )
-                );
-    }
-
-    private void actionWrite() {
-        System.out.print("명언 : ");
-        final String content = scanner.nextLine().trim();
-        System.out.print("작가 : ");
-        final String authorName = scanner.nextLine().trim();
-
-        final long id = ++lastQuotationId;
-
-        Quotation quotation = new Quotation(id, authorName, content);
-        quotations.add(quotation);
-
-        System.out.println("%d번 명언이 등록 되었습니다.".formatted(id));
-    }
 }
