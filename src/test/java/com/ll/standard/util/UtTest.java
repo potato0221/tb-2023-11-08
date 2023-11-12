75d035f721b735e0d286779d36fab1af8da6f659
potato0221 <wnqo10@naver.com>
2023-11-12 16:36:32 +0900

    file t4

--- src/test/java/com/ll/standard/util/UtTest.java
+++ src/test/java/com/ll/standard/util/UtTest.java
@@ -51,6 +51,16 @@ public class UtTest {
         Ut.file.save(test2FilePath, 100);
         final long age = Ut.file.getContentsAsLong(test2FilePath,0);
         assertThat(age).isEqualTo(100);
+        Ut.file.delete(test2FilePath);
+    }
+
+    @Test
+    @DisplayName("없는 파일 읽으라는 시도를 하면 null 반환")
+    void t4() {
+        final String content = Ut.file.getContent("temp/not-exist.txt");
+
+        assertThat(content).isNull();
+
     }
 
 }
