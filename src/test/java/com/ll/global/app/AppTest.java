130fecd97b4754b6801be211a915237a8f34f0bd
potato0221 <wnqo10@naver.com>
2023-11-12 13:51:46 +0900

    t12

--- src/test/java/com/ll/global/app/AppTest.java
+++ src/test/java/com/ll/global/app/AppTest.java
@@ -232,9 +232,27 @@ public class AppTest {
         assertThat(out)
                 .contains("3번 명언은 존재하지 않습니다.")
                 .contains("2번 명언이 삭제 되었습니다.");
+    }
+    @Test
+    @DisplayName("존재하지 않는 명언에 대한 수정 예외처리")
+    void t12() {
 
+        String out = run("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                등록
+                과거에 집착하지 마라.
+                홍길동
+                수정?id=2
+                과거에 집착하지 마라!
+                홍길동님
+                수정?id=3
+                """);
 
-
+        assertThat(out)
+                .contains("3번 명언은 존재하지 않습니다.")
+                .contains("2번 명언이 수정 되었습니다.");
     }
 
 
