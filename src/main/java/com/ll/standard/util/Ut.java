6301546aaba4b8f41666f879b87b93450bd4e64c
potato0221 <wnqo10@naver.com>
2023-11-12 16:14:48 +0900

    t2, pass

--- src/main/java/com/ll/standard/util/Ut.java
+++ src/main/java/com/ll/standard/util/Ut.java
@@ -13,13 +13,13 @@ public class Ut {
 
         @SneakyThrows
         public static void save(String filePath, String content) {
-            Path path = Paths.get(filePath);
+            final Path path = Paths.get(filePath);
 
             try {
                 Files.writeString(path, content, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
             } catch (IOException e) {
                 // 부모 디렉토리가 없어서 발생한 예외인지 확인합니다.
-                Path parentDir = path.getParent();
+                final Path parentDir = path.getParent();
                 if (parentDir != null && Files.notExists(parentDir)) {
                     Files.createDirectories(parentDir);
                     // 디렉토리를 생성한 후 다시 시도합니다.
@@ -38,6 +38,10 @@ public class Ut {
         @SneakyThrows
         public static void delete(String filePath) {
                 Files.delete(Paths.get(filePath));
+        }
+        @SneakyThrows
+        public static String getContents(String filePath) {
+            return Files.readString(Paths.get(filePath));
         }
     }
 }
--- src/test/java/com/ll/standard/util/UtTest.java
+++ src/test/java/com/ll/standard/util/UtTest.java
@@ -8,12 +8,27 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class UtTest {
 
     @Test
-    @DisplayName("파일 생성")
+    @DisplayName("파일 생성, 삭제")
     void t1(){
         String filePath="temp/test.txt";
         Ut.file.save(filePath,"내용");
 
         assertThat(Ut.file.exists(filePath)).isTrue();
+
+        Ut.file.delete(filePath);
+
+    }
+
+    @Test
+    @DisplayName("파일 내용 읽기")
+    void t2(){
+        String filePath="temp/test.txt";
+        Ut.file.save(filePath,"내용");
+
+
+        final String content=Ut.file.getContents(filePath);
+
+        assertThat(content).isEqualTo("내용");
 
         Ut.file.delete(filePath);
 
