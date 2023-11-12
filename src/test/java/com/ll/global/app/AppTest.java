5c642e2e5b922275dc0bd469bbc2d041ce8cbf73
potato0221 <wnqo10@naver.com>
2023-11-12 11:41:37 +0900

    t9

--- src/test/java/com/ll/global/app/AppTest.java
+++ src/test/java/com/ll/global/app/AppTest.java
@@ -161,6 +161,31 @@ public class AppTest {
 
     }
 
+    @Test
+    @DisplayName("삭제2")
+    void t9() {
+
+        String out = run("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                등록
+                과거에 집착하지 마라.
+                홍길동
+                삭제?id=2
+                목록
+                """);
+
+        assertThat(out)
+                .contains("번호 / 작가 / 명언")
+                .contains("--------------------")
+                .doesNotContain("2 / 홍길동 / 과거에 집착하지 마라.")
+                .contains("1번 명언이 삭제 되었습니다.")
+                .contains("1 / 작자미상 / 현재를 사랑하라.");
+
+
+    }
+
 
 
 
